48789cb7378feeb1471bb709572fc780a3a9ecf5
sanketh908 <sankeths908@gmail.com>
2026-03-18 18:47:09 +0530

    new commit

--- src/main/java/com/sanketh/AIChatBot/Service/ChatService.java
+++ src/main/java/com/sanketh/AIChatBot/Service/ChatService.java
@@ -1,8 +1,14 @@
 package com.sanketh.AIChatBot.Service;
 
+import com.sanketh.AIChatBot.Repository.ChatRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
+ private final ChatRepository chatRepository;
+
+    public ChatService(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
 
 }
