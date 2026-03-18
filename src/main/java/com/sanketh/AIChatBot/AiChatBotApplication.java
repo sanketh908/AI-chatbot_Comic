875854ab9bf15a9387add7833938304c1cb66475
sanketh908 <sankeths908@gmail.com>
2026-03-18 17:47:01 +0530

    new

---
+++ src/main/java/com/sanketh/AIChatBot/AiChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.sanketh.AIChatBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanketh/AIChatBot/AiChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanketh.AIChatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
