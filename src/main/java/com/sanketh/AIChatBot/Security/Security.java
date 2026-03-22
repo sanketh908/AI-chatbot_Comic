054da8e137cc2dcfa94e8b8ef3c00505cd24ff50
sanketh908 <sankeths908@gmail.com>
2026-03-22 09:55:07 +0530

    Admin impl

--- src/main/java/com/sanketh/AIChatBot/Security/Security.java
+++ src/main/java/com/sanketh/AIChatBot/Security/Security.java
@@ -2,8 +2,10 @@ package com.sanketh.AIChatBot.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,15 +19,20 @@ public class Security  {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->auth.requestMatchers("/chat/**").hasRole("ROLE_USER")
-                .requestMatchers("/admin/**").hasRole("ROLE_ADMIN").requestMatchers("/home/**").permitAll()
-                .anyRequest().authenticated())
-                .formLogin(form->form.loginPage("/home/register").permitAll())
+                .requestMatchers("/admin/**").hasRole("ROLE_ADMIN").requestMatchers("/home/**").permitAll().requestMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .logout(LogoutConfigurer::permitAll);
+
+
                 return http.build();
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+   public DaoAuthenticationProvider  daoAuthenticationProvider() {
 
+   }
 }
