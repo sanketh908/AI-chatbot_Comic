f7a7d6b63d74cfb4447b92abd8568f88558c9ca8
sanketh908 <sankeths908@gmail.com>
2026-03-22 07:12:16 +0530

    Admin impl

--- src/main/java/com/sanketh/AIChatBot/Security/UserServiceImpl.java
+++ src/main/java/com/sanketh/AIChatBot/Security/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.sanketh.AIChatBot.Security;
 
 import com.sanketh.AIChatBot.Entity.User;
 import com.sanketh.AIChatBot.Repository.UserRepository;
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
