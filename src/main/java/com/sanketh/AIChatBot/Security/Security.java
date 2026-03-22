ca543a728312bece8c17ed1472dfd11293c5b0d2
sanketh908 <sankeths908@gmail.com>
2026-03-22 12:57:59 +0530

    security impl

--- src/main/java/com/sanketh/AIChatBot/Security/Security.java
+++ src/main/java/com/sanketh/AIChatBot/Security/Security.java
@@ -2,6 +2,7 @@ package com.sanketh.AIChatBot.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration
 public class Security  {
+    private final UserServiceImpl userServiceImpl;
+
+    public Security(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -32,7 +39,11 @@ public class Security  {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-   public DaoAuthenticationProvider  daoAuthenticationProvider() {
-
+   @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userServiceImpl);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
    }
 }
