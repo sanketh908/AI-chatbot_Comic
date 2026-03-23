c57127c38f093604059e3edfb167d1598b0a129e
sanketh908 <sankeths908@gmail.com>
2026-03-23 16:05:52 +0530

    security impl

--- src/main/java/com/sanketh/AIChatBot/Security/UserServiceImpl.java
+++ src/main/java/com/sanketh/AIChatBot/Security/UserServiceImpl.java
@@ -12,10 +12,10 @@ public class UserServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
         if (user == null) {
-            throw new UsernameNotFoundException("User not found with username: " + username);
+            throw new UsernameNotFoundException("User not found with Email: " +email);
         } else {
             return new UserPrinciple(user);
         }
