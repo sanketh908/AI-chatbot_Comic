709e7efd7a17eeb850ffe397c9e74594a451a806
sanketh908 <sankeths908@gmail.com>
2026-03-26 06:49:33 +0530

    Adding GlobalExceptionHandler

--- src/main/java/com/sanketh/AIChatBot/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/sanketh/AIChatBot/Exception/GlobalExceptionHandler.java
@@ -1,16 +1,17 @@
 package com.sanketh.AIChatBot.Exception;
 
 import com.sanketh.AIChatBot.DTO.ResponseStructure;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public class GlobalExceptionHandler {
    @ExceptionHandler(UserNotFoundException.class)
    public ResponseStructure handleUserNotFoundException(UserNotFoundException ex) {
-       ResponseStructure response = new ResponseStructure();
-       response.setStatusCode(404);
-       response.setMessage(ex.getMessage());
-       return response;
+       return new ResponseStructure(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+   }
+   public ResponseStructure handleRegistrationFailedException(RegistrationFailedException ex) {
+       return new ResponseStructure(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
    }
 }
 
