970e25e5c982b2d5e7dde35697f8705ab546b9db
sanketh908 <sankeths908@gmail.com>
2026-03-29 18:47:11 +0530

    added StateFull response

--- src/main/java/com/sanketh/AIChatBot/Controller/ChatbotController.java
+++ src/main/java/com/sanketh/AIChatBot/Controller/ChatbotController.java
@@ -1,80 +1,80 @@
-package com.sanketh.AIChatBot.Controller;
+    package com.sanketh.AIChatBot.Controller;
 
-import com.sanketh.AIChatBot.DTO.PromptResponse;
-import com.sanketh.AIChatBot.DTO.Response;
-import com.sanketh.AIChatBot.Exception.ChatResponseGenerationException;
-import com.sanketh.AIChatBot.Exception.NothingToDeleteException;
-import com.sanketh.AIChatBot.Service.ChatService;
-import com.sanketh.AIChatBot.Service.ThinkingService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import com.sanketh.AIChatBot.DTO.PromptResponse;
+    import com.sanketh.AIChatBot.DTO.Response;
+    import com.sanketh.AIChatBot.Exception.ChatResponseGenerationException;
+    import com.sanketh.AIChatBot.Exception.NothingToDeleteException;
+    import com.sanketh.AIChatBot.Service.ChatService;
+    import com.sanketh.AIChatBot.Service.ThinkingService;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+    import java.util.List;
 
-@RestController
-@RequestMapping("/chat")
-@CrossOrigin(origins = "*")
-public class ChatbotController {
+    @RestController
+    @RequestMapping("/chat")
+    @CrossOrigin(origins = "*")
+    public class ChatbotController {
 
-    private final ChatService chatService;
-    private final ThinkingService thinkingService;
+        private final ChatService chatService;
+        private final ThinkingService thinkingService;
 
 
-    public ChatbotController(ChatService chatService, ThinkingService thinkingService) {
-        this.chatService = chatService;
-        this.thinkingService = thinkingService;
-    }
-    @GetMapping("/history")
-    public ResponseEntity<List<PromptResponse>> getHistory() {
-        List<PromptResponse> history = chatService.getHistory();
-        if (!history.isEmpty()) {
-            return new ResponseEntity<>(history, HttpStatus.OK);
+        public ChatbotController(ChatService chatService, ThinkingService thinkingService) {
+            this.chatService = chatService;
+            this.thinkingService = thinkingService;
+        }
+        @GetMapping("/history")
+        public ResponseEntity<List<PromptResponse>> getHistory() {
+            List<PromptResponse> history = chatService.getHistory();
+            if (!history.isEmpty()) {
+                return new ResponseEntity<>(history, HttpStatus.OK);
+
+            }
+            else
+               throw new NothingToDeleteException("Nothing to delete");
+
 
         }
-        else
-           throw new NothingToDeleteException("Nothing to delete");
 
-
-    }
-
-    @GetMapping("/response/stateless")
-    public ResponseEntity<Response> chat(@RequestParam("prompt") String prompt) {
-        String response = chatService.getResponse(prompt, prompt);
-        if (response != null) {
-            return new ResponseEntity<>(new Response(response), HttpStatus.OK);
+        @GetMapping("/response/stateless")
+        public ResponseEntity<Response> chat(@RequestParam("prompt") String prompt) {
+            String response = chatService.getResponse(prompt, prompt);
+            if (response != null) {
+                return new ResponseEntity<>(new Response(response), HttpStatus.OK);
+            }
+            else
+               throw new ChatResponseGenerationException("Failed to generate response");
         }
-        else
-           throw new ChatResponseGenerationException("Failed to generate response");
-    }
-    @GetMapping("/response/statefull")
-    public ResponseEntity<Response> thinkingMode(@RequestParam("prompt") String prompt) {
-        String response = thinkingService.getThinkingResponse(prompt);
-        if (response != null) {
-            return new ResponseEntity<>(new Response(response), HttpStatus.OK);
+        @GetMapping("/response/statefull")
+        public ResponseEntity<Response> thinkingMode(@RequestParam("prompt") String prompt) {
+            String response = thinkingService.getThinkingResponse(prompt);
+            if (response != null) {
+                return new ResponseEntity<>(new Response(response), HttpStatus.OK);
+            }
+            else {
+                throw new ChatResponseGenerationException("Failed to generate response");
+            }
         }
-        else {
-            throw new ChatResponseGenerationException("Failed to generate response");
+        @PostMapping("/deletehistory/{id}")
+        public ResponseEntity<String> deleteHistory(@PathVariable("id") Integer id) {
+            chatService.deleteHistoryBytId(id);
+            return new ResponseEntity<>("deleted successfully", HttpStatus.OK);
+
+        }
+        @PostMapping("/clearAllhistory")
+        public ResponseEntity<?> clearAllHistory() {
+         boolean deleted =   chatService.deleteAllHistory();
+         if(deleted){
+             return new ResponseEntity<>("All history cleared successfully", HttpStatus.OK);
+         }
+            else
+               throw new NothingToDeleteException("Nothing to delete");
+        }
+
+        @GetMapping("/ping")
+        public ResponseEntity<String> ping() {
+            return new ResponseEntity<>("Ok", HttpStatus.OK);
         }
     }
-    @PostMapping("/deletehistory/{id}")
-    public ResponseEntity<String> deleteHistory(@PathVariable("id") Integer id) {
-        chatService.deleteHistoryBytId(id);
-        return new ResponseEntity<>("deleted successfully", HttpStatus.OK);
-
-    }
-    @PostMapping("/clearAllhistory")
-    public ResponseEntity<?> clearAllHistory() {
-     boolean deleted =   chatService.deleteAllHistory();
-     if(deleted){
-         return new ResponseEntity<>("All history cleared successfully", HttpStatus.OK);
-     }
-        else
-           throw new NothingToDeleteException("Nothing to delete");
-    }
-
-    @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
-        return new ResponseEntity<>("Ok", HttpStatus.OK);
-    }
-}
