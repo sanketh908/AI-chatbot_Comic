51fc0b6f4fc1136b1dcddce53ea9f0d57d8077bb
sanketh908 <sankeths908@gmail.com>
2026-04-01 16:52:50 +0530

    all working fine

--- src/main/java/com/sanketh/AIChatBot/Controller/HomeController.java
+++ src/main/java/com/sanketh/AIChatBot/Controller/HomeController.java
@@ -15,14 +15,11 @@ import org.springframework.boot.autoconfigure.pulsar.PulsarProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+
 
 
 @RestController
--- src/main/java/com/sanketh/AIChatBot/Controller/UserController.java
+++ src/main/java/com/sanketh/AIChatBot/Controller/UserController.java
@@ -5,10 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/user")
 @CrossOrigin(origins = "*")
 public class UserController {
     private final UserService userService;
